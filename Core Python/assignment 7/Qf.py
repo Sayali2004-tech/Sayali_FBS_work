bffa9afc1981dd45d5f88ab68659fbed1e23aa4d
Sayali2004-tech <lawandesayali@gmail.com>
2025-11-30 22:46:07 +0530

    Delete Core Python/assignment 7 directory

--- Core Python/assignment 7/Qf.py
+++
@@ -1,9 +0,0 @@
-n = 5
-for i in range(1, n+1):
-    if i == 1:
-        for j in range(1, n+1):
-            print(j, end=" ")
-        print()
-    else:
-        print(" " * 4 * (i - 1), end="")
-        print(i)
