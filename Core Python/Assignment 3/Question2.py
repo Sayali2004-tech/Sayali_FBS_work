9485b649f63524bde82ec7b22bd5ce582d876552
Sayali2004-tech <lawandesayali@gmail.com>
2025-11-30 22:43:59 +0530

    Delete Core Python/Assignment 3 directory

--- Core Python/Assignment 3/Question2.py
+++
@@ -1,6 +0,0 @@
-ch = input("Enter an alphabet: ")
-
-if ch.lower() in ['a', 'e', 'i', 'o', 'u']:
-    print("Vowel")
-else:
-    print("Consonant")
--- Core Python/Assignment 3/Question7.py
+++
@@ -1,8 +0,0 @@
-correct_userid = "admin"
-correct_password = "12345"
-userid = input("Enter User ID: ")
-password = input("Enter Password: ")
-if userid == correct_userid and password == correct_password:
-    print("Login Successful!")
-else:
-    print("Invalid User ID or Password.")
